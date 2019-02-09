b771fc69051a244458bc2e688ee40f5f9f4857b0
蒼時弦也 <elct9620@frost.tw>
2019-02-09 20:15:48 +0800

    Add README.md template

--- lib/rb5/builder.rb
+++ lib/rb5/builder.rb
@@ -3,6 +3,10 @@
 module Rb5
   # :nodoc:
   class Builder < Rails::AppBuilder
+    def readme
+      template 'README.md.erb', 'README.md'
+    end
+
     def gemfile
       template 'Gemfile.erb', 'Gemfile'
     end
