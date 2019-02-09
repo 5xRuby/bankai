cd149f7f0dd260d21ed761c7963eaed02d4b7915
蒼時弦也 <elct9620@frost.tw>
2019-02-09 20:24:08 +0800

    Add .gitignore template

--- lib/rb5/builder.rb
+++ lib/rb5/builder.rb
@@ -7,6 +7,10 @@ module Rb5
       template 'README.md.erb', 'README.md'
     end
 
+    def gitignore
+      template 'gitignore.erb', '.gitignore'
+    end
+
     def gemfile
       template 'Gemfile.erb', 'Gemfile'
     end
