5b63313113b5f10d8bedb28dd8a358e4a4f9c004
蒼時弦也 <elct9620@frost.tw>
2020-12-07 18:12:38 +0800

    Allow Rails 5.2+ to user Bankai

--- lib/bankai/version.rb
+++ lib/bankai/version.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module Bankai
-  VERSION = '0.8.0'
+  VERSION = '0.8.1'
   RUBY_VERSION = '2.6.6'
-  RAILS_VERSION = '6.0.3'
+  RAILS_VERSION = '5.2.0'
   RUBOCOP_VERSION = '1.5.1'
   CAPISTRANO_VERSION = '3.14.1'
 end
