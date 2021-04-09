aaded33cf1048d0f12ddf03acc216637c9641c15
蒼時弦也 <elct9620@frost.tw>
2021-04-09 12:09:39 +0800

    Bump version into 0.10.0

--- lib/bankai/version.rb
+++ lib/bankai/version.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module Bankai
-  VERSION = '0.9.1'
-  RUBY_VERSION = '2.6.6'
+  VERSION = '0.10.0'
+  RUBY_VERSION = '2.6.7'
   RAILS_VERSION = '5.2.0'
   RUBOCOP_VERSION = '1.8.1'
   CAPISTRANO_VERSION = '3.14.1'
