d2d5f405a63efed44e55747590baa99aa8f0dd49
蒼時弦也 <elct9620@frost.tw>
2021-04-09 12:05:40 +0800

    Add simplecov coverage report to GitLab CI

--- templates/spec_helper.rb
+++ templates/spec_helper.rb
@@ -2,6 +2,10 @@
 
 if ENV.fetch('COVERAGE', false) || ENV.fetch('CI', false)
   require 'simplecov'
+  require 'simplecov-cobertura'
+
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter if ENV.fetch('GITLAB_CI', false)
+
   SimpleCov.start 'rails' do
     add_filter 'vendor'
   end
