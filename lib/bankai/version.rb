894376ba6cefc7b1fe9c822490305d71ead94b99
蒼時弦也 <elct9620@frost.tw>
2021-08-30 15:33:58 +0800

    Bump version into 0.12.0

--- lib/bankai/version.rb
+++ lib/bankai/version.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module Bankai
-  VERSION = '0.11.0'
+  VERSION = '0.12.0'
   RUBY_VERSION = '2.6.7'
   RAILS_VERSION = '5.2.0'
-  RUBOCOP_VERSION = '1.8.1'
-  CAPISTRANO_VERSION = '3.14.1'
+  RUBOCOP_VERSION = '1.9.1'
+  CAPISTRANO_VERSION = '3.16.0'
 end
