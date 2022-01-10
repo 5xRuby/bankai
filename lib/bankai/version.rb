12edea0cf23b70b8b4cd9e77750332935ec4e521
蒼時弦也 <elct9620@frost.tw>
2022-01-10 16:39:00 +0800

    Update Gemfile.erb to Rails 7 as default

--- lib/bankai/version.rb
+++ lib/bankai/version.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module Bankai
-  VERSION = '0.12.1'
+  VERSION = '0.13.0'
   RUBY_VERSION = '2.6.7'
-  RAILS_VERSION = '5.2.0'
-  RUBOCOP_VERSION = '1.9.1'
+  RAILS_VERSION = '7.0.0'
+  RUBOCOP_VERSION = '1.24.1'
   CAPISTRANO_VERSION = '3.16.0'
 end
